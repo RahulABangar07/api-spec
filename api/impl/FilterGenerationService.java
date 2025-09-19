372c3e1f49c359cb4fab9d07892e6fad0743902a
RahulABangar07 <rahul.bangar7@gmail.com>
2025-09-19 10:09:20 +0530

    Update FilterGenerationService.java

--- api/impl/FilterGenerationService.java
+++ api/impl/FilterGenerationService.java
@@ -1,14 +1,23 @@
 package com.example.filter;
 
+import com.azure.search.documents.models.SearchOptions;
 import java.util.List;
 
-public class FilterGenerationServicev1 {
+public class FilterGenerationService {
 
+    // Cosmos DB query
     public String generateCosmosQuery(List<FilterDetail> filters) {
         return CosmosQueryBuilder.buildQuery(filters);
     }
 
-    public String generateAzureSearchQuery(List<FilterDetail> filters) {
-        return AzureSearchQueryBuilder.buildQuery(filters);
+    // Azure Search: build SearchOptions with filter
+    public SearchOptions generateAzureSearchOptions(List<FilterDetail> filters) {
+        String filter = AzureSearchQueryBuilder.buildQuery(filters);
+        SearchOptions options = new SearchOptions();
+
+        if (filter != null && !filter.isEmpty()) {
+            options.setFilter(filter);
+        }
+        return options;
     }
 }
