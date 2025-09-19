896e6e2cee081f9a433d5e18ae1c88037fa0dd20
RahulABangar07 <rahul.bangar7@gmail.com>
2025-09-19 06:27:48 +0530

    Create FilterAggregator.java

---
+++ model/FilterAggregator.java
@@ -0,0 +1,10 @@
+package com.example.campaign.dto;
+
+import java.util.List;
+
+public class FilterAggregator {
+    private String type;
+    private List<Filter> filters;
+
+    // Getters and Setters
+}
