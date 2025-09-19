7b0e6f6160c12e9f5ab360dfd1bfb1baafd02a5c
RahulABangar07 <rahul.bangar7@gmail.com>
2025-09-19 06:26:41 +0530

    Create ReviewedCampaign.java

---
+++ model/ReviewedCampaign.java
@@ -0,0 +1,14 @@
+package com.example.campaign.dto;
+
+import java.time.OffsetDateTime;
+
+public class ReviewedCampaign extends Campaign {
+    private String status;
+    private String reviewComments;
+    private String reviewer1;
+    private OffsetDateTime reviewer1Date;
+    private String reviewer2;
+    private OffsetDateTime reviewer2Date;
+
+    // Getters and Setters
+}
